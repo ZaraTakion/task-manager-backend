9b4dbdd0f23ce897e46143e81c8a78732b273c1e
Rodrigo(Zara) <rodzmaciel21@gmail.com>
2026-01-18 15:37:24 -0300

    feat: add task creation and listing endpoints

--- app/main.py
+++ app/main.py
@@ -1,8 +1,26 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
+
+class Task(BaseModel):
+    title: str
+    completed: bool = False
+
+tasks = []
 
 
 @app.get("/")
 def read_root():
     return {"message": "API do Task Manager está viva"}
+
+
+@app.post("/tasks")
+def create_task(task: Task):
+    tasks.append(task)
+    return task
+
+
+@app.get("/tasks")
+def list_tasks():
+    return tasks
